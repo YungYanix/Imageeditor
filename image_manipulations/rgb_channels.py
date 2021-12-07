afee799cf0d7cc92402dba6bddd8b327e2a084cb
yanis <y.skripnikov@outlook.com>
2021-12-07 17:49:45 +0300

    Saturation and value added

--- image_manipulations/rgb_channels.py
+++ image_manipulations/rgb_channels.py
@@ -8,7 +8,8 @@ def rgb_channels(state,block):
     b = block.slider('Blue', value= default, min_value=0, max_value=255)
     try:
         img[:, :, 0] += r - default
-    except UFuncTypeError
+    except UFuncTypeError:
+
     img[:, :, 1] += g - default
     img[:, :, 2] += b - default
     state['image'] = img
